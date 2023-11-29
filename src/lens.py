f704cd88dad9109c5daa3d467fdc2deebba46ef0
DE IM <ekdmss0202@gachon.ac.kr>
2023-11-30 04:14:34 +0900

    add filters

---
+++ src/lens.py
@@ -0,0 +1,44 @@
+import cv2
+import image_processor
+import numpy as np
+import save_image
+
+img = image_processor.read_selected_image()    # 이미지 읽기
+print(img.shape)
+rows, cols = img.shape[:2]
+
+# ---① 설정 값 셋팅
+exp = 2       # 볼록, 오목 지수 (오목 : 0.1 ~ 1, 볼록 : 1.1~)
+scale = 1           # 변환 영역 크기 (0 ~ 1)
+
+# 매핑 배열 생성 ---②
+mapy, mapx = np.indices((rows, cols),dtype=np.float32)
+
+# 좌상단 기준좌표에서 -1~1로 정규화된 중심점 기준 좌표로 변경 ---③
+mapx = 2*mapx/(cols-1)-1
+mapy = 2*mapy/(rows-1)-1
+
+# 직교좌표를 극 좌표로 변환 ---④
+r, theta = cv2.cartToPolar(mapx, mapy)
+
+# 왜곡 영역만 중심확대/축소 지수 적용 ---⑤
+r[r< scale] = r[r<scale] **exp  
+
+# 극 좌표를 직교좌표로 변환 ---⑥
+mapx, mapy = cv2.polarToCart(r, theta)
+
+# 중심점 기준에서 좌상단 기준으로 변경 ---⑦
+mapx = ((mapx + 1)*cols-1)/2
+mapy = ((mapy + 1)*rows-1)/2
+# 재매핑 변환
+distorted = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
+
+img = cv2.resize(img, (580, 400))
+distorted = cv2.resize(distorted, (580, 400))
+
+merge_image = cv2.hconcat([img, distorted])
+cv2.imshow('lens', merge_image)
+
+cv2.waitKey()
+save_image.save_image(img)
+cv2.destroyAllWindows()
--- src/main.py
+++ src/main.py
@@ -6,7 +6,13 @@ def run_filter1():
 
 def run_filter2():
     subprocess.run(["python", "liquify.py"])
-    
+
+def run_filter3():
+    subprocess.run(["python", "lens.py"])
+
+def run_filter4():
+    subprocess.run(["python", "wave.py"])
+
 def on_closing():
     root.destroy()
 
@@ -23,5 +29,10 @@ filter1_button.pack()
 filter2_button = tk.Button(root, text="리퀴파이", command=run_filter2)
 filter2_button.pack()
 
+filter3_button = tk.Button(root, text="볼록렌즈", command=run_filter3)
+filter3_button.pack()
+
+filter4_button = tk.Button(root, text="물결필터", command=run_filter4)
+filter4_button.pack()
 
 root.mainloop()
