f94f4b6d850d72d4e92363d701e48ef774a1322b
DE IM <ekdmss0202@gachon.ac.kr>
2023-12-08 04:00:29 +0900

    design

--- src/main.py
+++ src/main.py
@@ -21,23 +21,26 @@ def on_closing():
 
 root = tk.Tk()
 root.title("Image Editor")
+root.minsize(300, 300)
 root.protocol("WM_DELETE_WINDOW", on_closing)
 
-label = tk.Label(root, text="필터를 고르세요")
-label.pack()
+label = tk.Label(root, height=3, text="필터를 고르세요", font=5, fg="blue")
+label.pack(pady = 10)
 
-filter1_button = tk.Button(root, text="모자이크", command=run_filter1)
-filter1_button.pack()
+filter1_button = tk.Button(root, width=10, text="모자이크", command=run_filter1)
+filter1_button.pack(pady = 5)
 
-filter2_button = tk.Button(root, text="리퀴파이", command=run_filter2)
-filter2_button.pack()
+filter2_button = tk.Button(root, width=10, text="리퀴파이", command=run_filter2)
+filter2_button.pack(pady = 5)
 
-filter3_button = tk.Button(root, text="볼록렌즈", command=run_filter3)
-filter3_button.pack()
+filter3_button = tk.Button(root, width=10, text="볼록렌즈", command=run_filter3)
+filter3_button.pack(pady = 5)
 
-filter4_button = tk.Button(root, text="물결필터", command=run_filter4)
-filter4_button.pack()
+filter4_button = tk.Button(root, width=10, text="물결필터", command=run_filter4)
+filter4_button.pack(pady = 5)
 
-filter5_button = tk.Button(root, text="선 필터", command=run_filter5)
-filter5_button.pack()
+filter5_button = tk.Button(root, width=10, text="선 필터", command=run_filter5)
+filter5_button.pack(pady = 5)
+
+
 root.mainloop()
